5186f9320c54e21a3ebc83d2c88ca209f486def9
J2ghz <j2.00ghz@gmail.com>
2017-02-13 19:40:07 +0100

    Server template

--- A417Sync.Server/Program.cs
+++ A417Sync.Server/Program.cs
@@ -1,30 +1,25 @@
-﻿namespace A417Sync.Server
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace A417Sync.Server
 {
-    using System;
-    using System.IO;
-    using System.Linq;
-    using System.Threading;
-
-    using A417Sync.Core;
-
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            if (string.IsNullOrWhiteSpace(args.FirstOrDefault()))
-            {
-                var client = new Client(
-                    new DirectoryInfo(@"D:\417Launcher\417Launcher\417addons"),
-                    new Uri(@"https://addons.j2ghz.com/"));
-                var repo = Client.DownloadRepo(new Uri(@"https://addons.j2ghz.com/index.xml")).Result;
-                var actions = client.CollectActions(repo.Addons);
-                client.Update(actions, CancellationToken.None).GetAwaiter().GetResult();
-            }
-            else
-            {
-                var r = RepoFactory.MakeRepoDefaultModpack(new DirectoryInfo(args[0]));
-                RepoFactory.SaveRepo(r, Path.Combine(args[0], "index.xml"));
-            }
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
         }
     }
 }

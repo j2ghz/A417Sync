d90ddb0459002f88a255afd901209e05bf101a1d
J2ghz <j2.00ghz@gmail.com>
2017-02-13 19:31:30 +0100

    launch parameter generator

---
+++ A417Sync.Core/ArmaHelpers.cs
@@ -0,0 +1,16 @@
+﻿namespace A417Sync.Core
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    using A417Sync.Core.Models;
+
+    public static class ArmaHelpers
+    {
+        public static string AddonsLaunchParameter(IEnumerable<Addon> addons, string prefix)
+        {
+            return string.Join(";", addons.Select(addon => Path.Combine(prefix, addon.Name)));
+        }
+    }
+}

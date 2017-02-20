c2c5bcca85cd699ea2f46a03b6d6687997e01788
J2ghz <j2.00ghz@gmail.com>
2017-02-21 00:13:54 +0100

    HockeyApp

--- A417Sync.Client/App.xaml.cs
+++ A417Sync.Client/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.HockeyApp;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,12 @@ namespace A417Sync.Client
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            HockeyClient.Current.Configure("fcc1c7f687e344eb8e805ae492daf0c2");
+            HockeyClient.Current.SendCrashesAsync(true).GetAwaiter().GetResult();
+            HockeyClient.Current.TrackEvent("Launch");
+        }
     }
 }

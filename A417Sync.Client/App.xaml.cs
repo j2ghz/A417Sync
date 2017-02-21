e9e9e62d726b060f7b35f7ff41088ebb1c0039bf
J2ghz <j2.00ghz@gmail.com>
2017-02-21 13:05:03 +0100

    add hockeyapp, switch to partial trust

--- A417Sync.Client/App.xaml.cs
+++ A417Sync.Client/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using RollbarDotNet;
+﻿using Microsoft.HockeyApp;
+using RollbarDotNet;
 using System;
 using System.Windows;
 
@@ -27,7 +28,17 @@ namespace A417Sync.Client
                     Email = $"{Environment.UserName}@{Environment.UserDomainName}"
                 }
             );
-            AppDomain.CurrentDomain.UnhandledException += (sender, args) => Rollbar.Report(args.ExceptionObject as System.Exception);
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                Rollbar.Report(args.ExceptionObject as System.Exception);
+            };
+
+            HockeyClient.Current.Configure("fcc1c7f687e344eb8e805ae492daf0c2");
+            HockeyClient.Current.SendCrashesAsync(true).GetAwaiter().GetResult();
+            HockeyAppWorkaroundInitializer.InitializeAsync().GetAwaiter().GetResult();
+            HockeyClient.Current.TrackEvent("Launch");
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) => HockeyClient.Current.TrackException(args.ExceptionObject as System.Exception);
         }
     }
 }

c41f868dcb1004e62e348e2d6dd75293786b1fe8
J2ghz <j2.00ghz@gmail.com>
2017-02-22 17:09:16 +0100

    fixes

--- A417Sync.Client/App.xaml.cs
+++ A417Sync.Client/App.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.HockeyApp;
-using RollbarDotNet;
 using System;
 using System.Windows;
 
@@ -15,29 +14,15 @@ namespace A417Sync.Client
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            Rollbar.Init(new RollbarConfig
-            {
-                AccessToken = "d5d2bfe7fa624f2790ed45bd2a2d8d96",
-#if DEBUG
-                Environment = "Debug"
-#else
-                Environment = "Release"
-#endif
-            });
-            Rollbar.PersonData(() => new Person($"{Environment.UserName}@{Environment.CurrentDirectory}@{Environment.MachineName}".GetHashCode().ToString())
-            {
-                    UserName = Environment.UserName,
-                    Email = $"{Environment.UserName}@{Environment.UserDomainName}"
-            });
-            Rollbar.Report("Launch");
-            AppDomain.CurrentDomain.UnhandledException += (sender, args) => Rollbar.Report(args.ExceptionObject as System.Exception);
 
             HockeyClient.Current.Configure("fcc1c7f687e344eb8e805ae492daf0c2");
             HockeyClient.Current.SendCrashesAsync(true).GetAwaiter();
-            HockeyAppWorkaroundInitializer.InitializeAsync().GetAwaiter().GetResult();
+            ////HockeyAppWorkaroundInitializer.InitializeAsync().GetAwaiter().GetResult();
             HockeyClient.Current.TrackEvent("Launch");
 
             AppDomain.CurrentDomain.UnhandledException += (sender, args) => HockeyClient.Current.TrackException(args.ExceptionObject as System.Exception);
+
+            HockeyClient.Current.Flush();
         }
     }
 }

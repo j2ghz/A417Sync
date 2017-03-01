07f4c36859d302644bf66efd5c0ee2f56fb529b1
J2ghz <j2.00ghz@gmail.com>
2017-03-01 22:39:37 +0100

    Change log verbosity

--- A417Sync.Client/Client.cs
+++ A417Sync.Client/Client.cs
@@ -104,7 +104,7 @@
 
             if (local.LastWriteTimeUtc.CompareTo(remote.LastChange) != 0)
             {
-                Log.Verbose("Scheduling {file} to download, date is different: {@local} vs {@remote}", local.Name, local.LastWriteTimeUtc, remote.LastChange);
+                Log.Information("Scheduling {file} to download, date is different: {@local} vs {@remote}", local.Name, local.LastWriteTimeUtc, remote.LastChange);
                 return new Download(local, remote, addon, this.RepoRootUri, remote.LastChange)
                            {
                                Action =

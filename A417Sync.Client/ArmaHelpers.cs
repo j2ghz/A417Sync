edaf6edfd9cedbfb0feb49d0c76efa491c23a1d4
J2ghz <j2.00ghz@gmail.com>
2017-03-02 21:48:56 +0100

    Better arma start

--- A417Sync.Client/ArmaHelpers.cs
+++ A417Sync.Client/ArmaHelpers.cs
@@ -1,5 +1,6 @@
 ﻿namespace A417Sync.Core
 {
+    using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
@@ -7,10 +8,31 @@
     using A417Sync.Core.Models;
 
     using DerAtrox.Arma3LauncherLib.Model;
-    using DerAtrox.Arma3LauncherLib.Utilities;
+
+    using Microsoft.Win32;
 
     public static class ArmaHelpers
     {
+        public static string GetArma3Path()
+        {
+            try
+            {
+                RegistryKey registryKey =
+                    Registry.LocalMachine.OpenSubKey(
+                        Environment.Is64BitOperatingSystem ? "SOFTWARE\\Wow6432Node" : "SOFTWARE");
+                registryKey =
+                    registryKey.OpenSubKey(
+                            registryKey.GetSubKeyNames()
+                                .First(key => string.Equals(key, "Bohemia Interactive", StringComparison.OrdinalIgnoreCase)))
+                        .OpenSubKey("arma 3");
+                return registryKey.GetValue("MAIN").ToString();
+            }
+            catch
+            {
+                throw new ArmaNotFoundException("Arma 3 installation directory could not be found!");
+            }
+        }
+
         public static void StartArma(IEnumerable<Addon> addons, DirectoryInfo basePath, IEnumerable<string> Arguments)
         {
             var addonFolders = basePath.EnumerateDirectories();
@@ -18,7 +40,19 @@
             settings.Mods =
                 addons.Select(addon => addonFolders.First(folder => folder.Name == addon.Name).FullName).ToList();
             settings.OtherArgs.AddRange(Arguments);
-            new ArmaLauncher().Connect(ArmaUtils.GetArma3Path(), settings, true);
+            new ArmaLauncher().Connect(Path.Combine(GetArma3Path(), "arma3battleye.exe"), settings, true);
+        }
+    }
+
+    internal class ArmaNotFoundException : Exception
+    {
+        public ArmaNotFoundException()
+        {
+        }
+
+        public ArmaNotFoundException(string message)
+            : base(message)
+        {
         }
     }
 }

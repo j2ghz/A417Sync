c2a9303019de15166eb8c1b2a8a5e00419a9183e
J2ghz <j2.00ghz@gmail.com>
2017-03-05 19:59:35 +0100

    Unobserved exception fix

--- A417Sync.Client/MainWindow.xaml.cs
+++ A417Sync.Client/MainWindow.xaml.cs
@@ -5,12 +5,12 @@
     using System.IO;
     using System.Linq;
     using System.Threading;
+    using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Threading;
 
     using Microsoft.HockeyApp;
-    using Microsoft.VisualBasic;
 
     using Serilog;
 
@@ -60,6 +60,12 @@
             new Feedback().ShowDialog();
         }
 
+        private void ChangeModpack(object sender, SelectionChangedEventArgs e)
+        {
+            this.ViewModel.CanStart = false;
+            this.ViewModel.CanDownload = false;
+        }
+
         private async void Check(object sender, RoutedEventArgs e)
         {
             var uri = new Uri(this.ViewModel.Url);
@@ -72,7 +78,7 @@
             await this.ViewModel.Client.CollectActions(
                 this.ViewModel.SelectedModpack.Addons.Select(
                     name => this.ViewModel.Repo.Addons.Find(addon => addon.Name == name)),
-                this.ViewModel.Actions);
+                this.ViewModel.Actions).ConfigureAwait(false);
             this.ViewModel.Recalculate();
             this.ViewModel.CanCheck = true;
             if (this.ViewModel.Actions.Any())
@@ -93,18 +99,16 @@
             this.ViewModel.Repo = await Client.DownloadRepo(uri).ConfigureAwait(false);
             this.ViewModel.SelectedModpack = this.ViewModel.Repo.Modpacks[0];
 
-            foreach (var m in this.ViewModel.Repo.Modpacks)
-            {
-                var serverInfo = ArmaHelpers.ServerInfo(m).GetServerInfoAsync();
-                serverInfo.ContinueWith(
-                    t =>
-                        {
-                            Log.Debug("{@server}", t.Result);
-                            return this.Dispatcher.BeginInvoke(
-                                DispatcherPriority.Background,
-                                new Action(() => this.ViewModel.Servers.Add(t.Result)));
-                        }).ConfigureAwait(false).GetAwaiter();
-            }
+            await Task.WhenAll(
+                this.ViewModel.Repo.Modpacks.Select(
+                    m => ArmaHelpers.ServerInfo(m).GetServerInfoAsync().ContinueWith(
+                        t =>
+                            {
+                                Log.Debug("{@server}", t.Result);
+                                return this.Dispatcher.InvokeAsync(
+                                    () => this.ViewModel.Servers.Add(t.Result),
+                                    DispatcherPriority.Background);
+                            }))).ConfigureAwait(false);
 
             this.ViewModel.CanLoadRepo = true;
             this.ViewModel.CanCheck = true;
@@ -128,12 +132,6 @@
                 this.ViewModel.Repo.Addons,
                 new DirectoryInfo(this.ViewModel.Path),
                 this.ViewModel.Params);
-        }
-
-        private void ChangeModpack(object sender, SelectionChangedEventArgs e)
-        {
-            this.ViewModel.CanStart = false;
-            this.ViewModel.CanDownload = false;
         }
     }
 }

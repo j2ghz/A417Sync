7d84ef90736b7b7008069e061797abafd97e31ea
J2ghz <j2.00ghz@gmail.com>
2017-03-06 21:47:45 +0100

    Fix #29

--- A417Sync.Client/App.xaml.cs
+++ A417Sync.Client/App.xaml.cs
@@ -111,25 +111,6 @@
         private void SetupLogging()
         {
             Directory.CreateDirectory(Path.GetDirectoryName(this.LogFile));
-            var config = LogglyConfig.Instance;
-            config.CustomerToken = "de5800d3-c799-4865-8f20-d8106467c08b";
-            config.ApplicationName = "A417Sync";
-            config.Transport = new TransportConfiguration()
-                                   {
-                                       EndpointHostname = "logs-01.loggly.com",
-                                       EndpointPort = 443,
-                                       LogTransport = LogTransport.Https
-                                   };
-            config.ThrowExceptions = true;
-
-            // Define Tags sent to Loggly
-            config.TagConfig.Tags.AddRange(
-                new ITag[]
-                    {
-                        new ApplicationNameTag { Formatter = "application-{0}" },
-                        new HostnameTag { Formatter = "host-{0}" },
-                        new OperatingSystemVersionTag { Formatter = "os-{0}" }
-                    });
 
             Log.Logger =
                 new LoggerConfiguration().WriteTo.LiterateConsole(
@@ -137,7 +118,6 @@
                     .WriteTo.RollingFile(
                         this.LogFile,
                         outputTemplate: "{Timestamp:o} [{Level:u3}] ({SourceContext}) {Message}{NewLine}{Exception}")
-                    .WriteTo.Loggly(bufferBaseFilename: Path.GetTempFileName())
                     .Enrich.FromLogContext()
                     .MinimumLevel.Verbose()
                     .CreateLogger();
